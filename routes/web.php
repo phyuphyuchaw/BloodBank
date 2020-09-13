3f6d47ddf8bd57a2dee449ceaf9211d5409b02bf
phyuphyuchaw <phyuphyuchaw60@gmail.com>
2020-09-13 22:53:47 +0630

    edit BackendController.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-    //return 'Hello Laravel';
+   // return view('welcome');
+    return 'Hello Laravel';
 });
 
 Route::get('/testing',function ($value=''){
 	return 'Thhis is a testing page!';
 });
+
+  Route::get('dashboard','BackendController@dashboardfun')->name('dashboardpage');
